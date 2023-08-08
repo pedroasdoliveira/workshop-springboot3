48e6b123e482650d3c31f714e0cbf5886177b0d1
Pedro Oliveira <Pedroaolive@gmail.com>
2023-08-08 19:23:58 -0300

    feat: add Order, Instants and ISO 8601

---
+++ src/main/java/com/springpedro/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.springpedro.course.repositories;
+
+import com.springpedro.course.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
