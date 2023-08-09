b492db6ef8f28888ad12e38d8d2f12b21bcffbb6
Pedro Oliveira <Pedroaolive@gmail.com>
2023-08-09 19:14:55 -0300

    feat: add many-to-many association with Products and Category & implements yours services and resources

---
+++ src/main/java/com/springpedro/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.springpedro.course.repositories;
+
+import com.springpedro.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
