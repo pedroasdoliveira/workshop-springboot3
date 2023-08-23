11283d6f032781d4ad2a64b16cd3ae6caffcec51
Pedro Oliveira <Pedroaolive@gmail.com>
2023-08-23 19:55:50 -0300

    feat: add User insert method in Service and Resource

--- src/main/java/com/springpedro/course/resources/UserResource.java
+++ src/main/java/com/springpedro/course/resources/UserResource.java
@@ -5,11 +5,10 @@ import com.springpedro.course.entities.User;
 import com.springpedro.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -31,5 +30,13 @@ public class UserResource {
         User userObj = service.findById(id);
 
         return ResponseEntity.ok().body(userObj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+         obj = service.insert(obj);
+         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(obj);
     }
 }
