37613a9ca032b408307cf4f1d558f765750f739d
Pedro Oliveira <Pedroaolive@gmail.com>
2023-08-23 18:01:36 -0300

    feat: add relation OneToMany with Order and OrderItems & create OrderItemRepository

--- src/main/java/com/springpedro/course/config/TestConfig.java
+++ src/main/java/com/springpedro/course/config/TestConfig.java
@@ -1,15 +1,9 @@
 package com.springpedro.course.config;
 
-import com.springpedro.course.entities.Category;
-import com.springpedro.course.entities.Order;
-import com.springpedro.course.entities.Product;
-import com.springpedro.course.entities.User;
+import com.springpedro.course.entities.*;
 import com.springpedro.course.entities.enuns.OrderStatus;
 
-import com.springpedro.course.repositories.CategoryRepository;
-import com.springpedro.course.repositories.OrderRepository;
-import com.springpedro.course.repositories.ProductRepository;
-import com.springpedro.course.repositories.UserRepository;
+import com.springpedro.course.repositories.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -34,6 +28,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -69,5 +66,12 @@ public class TestConfig implements CommandLineRunner {
         p5.getCategories().add(cat2);
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
--- src/main/java/com/springpedro/course/entities/OrderItem.java
+++ src/main/java/com/springpedro/course/entities/OrderItem.java
@@ -1,5 +1,6 @@
 package com.springpedro.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.springpedro.course.entities.pk.OrderItemPk;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
@@ -14,7 +15,7 @@ public class OrderItem implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @EmbeddedId
-    private OrderItemPk id;
+    private OrderItemPk id = new OrderItemPk();
 
     private Integer quantity;
     private Double price;
@@ -29,6 +30,7 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    @JsonIgnore
     public Order getOrder() {
         return id.getOrder();
     }
