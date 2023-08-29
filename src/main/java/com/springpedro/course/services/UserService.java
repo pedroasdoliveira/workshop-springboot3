fa47a076114850ebe58f617976a0920b952abcaa
Pedro Oliveira <Pedroaolive@gmail.com>
2023-08-29 18:13:37 -0300

    chore: add implementation Exception in method findById

--- src/main/java/com/springpedro/course/services/UserService.java
+++ src/main/java/com/springpedro/course/services/UserService.java
@@ -3,6 +3,7 @@ package com.springpedro.course.services;
 import com.springpedro.course.entities.User;
 import com.springpedro.course.repositories.UserRepository;
 
+import com.springpedro.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,7 +23,7 @@ public class UserService {
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id); // O metodo traz o obj do banco de dados como um todo
 
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User userObj) {
