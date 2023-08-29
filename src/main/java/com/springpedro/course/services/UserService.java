09a92af0e3f4133ebe21d9c3a2ac782f8d61ef11
Pedro Oliveira <Pedroaolive@gmail.com>
2023-08-29 18:33:01 -0300

    chore: add Exception Handler for delete user

--- src/main/java/com/springpedro/course/services/UserService.java
+++ src/main/java/com/springpedro/course/services/UserService.java
@@ -3,8 +3,11 @@ package com.springpedro.course.services;
 import com.springpedro.course.entities.User;
 import com.springpedro.course.repositories.UserRepository;
 
+import com.springpedro.course.services.exceptions.DatabaseException;
 import com.springpedro.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -44,6 +47,12 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 }
